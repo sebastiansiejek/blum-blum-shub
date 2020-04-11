2419e2fce6503878f896dc72d662c195efac40c8
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-04-11 22:35:49 +0200

    feat: add printPrimeAndCongruentNumbers

--- utils/printPrimeNumbers.py
+++ utils/printPrimeNumbers.py
@@ -1,7 +1,14 @@
 from utils.isPrime import isPrime
+from utils.isCongruentNumber import isCongruentNumber
 
 
 def printPrimeNumbers(max):
     for number in range(2, max):
         if(isPrime(number)):
+            print(number)
+
+
+def printPrimeAndCongruentNumbers(max):
+    for number in range(2, max):
+        if(isPrime(number) and isCongruentNumber(number)):
             print(number)
